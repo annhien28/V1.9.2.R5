bfc47b80b27aa8227d9ab8b7b9c26c2c77539ad9
annhien28 <141406289+annhien28@users.noreply.github.com>
2023-08-04 18:22:16 +0700

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-    ./astrominer -w dero1qysa3yzppx5nula3z7ylf9n3mgnhsx94ce209tca8gvqdnyl6e0wvqquq96rg -r nodent2.cpumining.cloud:10100 -r1 dero.rabidmining.com:10300 -r2 community-pools.mysrv.cloud:10300 -p rpc;
-    sleep 5;
-done

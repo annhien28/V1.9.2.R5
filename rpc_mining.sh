22b7db09226976602d7b55be0c6e639b7ef56139
annhien28 <141406289+annhien28@users.noreply.github.com>
2023-08-04 18:48:06 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ././astrominer -w dero1qysa3yzppx5nula3z7ylf9n3mgnhsx94ce209tca8gvqdnyl6e0wvqquq96rg -r nodent2.cpumining.cloud:10100 -r1 dero.rabidmining.com:10300 -r2 community-pools.mysrv.cloud:10300 -p rpc
+    ./astrominer -w dero1qysa3yzppx5nula3z7ylf9n3mgnhsx94ce209tca8gvqdnyl6e0wvqquq96rg -r dero.androidmining.xyz:10100 -r1 nodent2.cpumining.cloud:10100 -r2 dero.rabidmining.com:10300 -p rpc -m 5;
     sleep 5;
 done
